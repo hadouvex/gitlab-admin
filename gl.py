3034b07e209b0938543b4ea516893f03f2edd88c
Georgy <georgy.makarov@outlook.com>
2022-01-18 12:01:21 +0300

    ...

--- gl.py
+++ gl.py
@@ -10,7 +10,8 @@ from env import env
 
 DEFAULT_GROUPS_JSON_FILE="groups"
 DEFAULT_PROJECTS_JSON_FILE="projects"
-DEFAULT_PROJECTS_UNIQUE_USERS_IDS_FILE="unique_users_ids"
+DEFAULT_UNIQUE_USERS_IDS_FILE="unique_users_ids"
+DEFAULT_USERS_IDS_FILE="unique_users_specific_attributes"
 
 GITLAB_API_USERS_URL_BASE="https://gitlab.com/api/v4/users"
 GITLAB_API_PROJECTS_URL_BASE="https://gitlab.com/api/v4/projects"
@@ -45,7 +46,7 @@ def write_to_json(data, file_name):
         print(f"couldn't find {file_name} file, created new: {file_name}.json")
 
     with open(f'{file_name}.json', 'w') as f:
-            json.dump(data, f)
+            json.dump(data, f, ensure_ascii=False)
 
 def read_from_json(file):
     with open(f"{file}.json") as f:
@@ -106,7 +107,7 @@ def write_all_groups_to_memcached(visibility=DEFAULT_VISIBILITY):
         groups_dict[g.path] = g.id
     write_many_to_memcached(groups_dict)
 
-def get_unique_user_logins_from_groups():
+def get_unique_user_ids_from_groups():
     ids_list = get_groups_ids_from_json()
     users_ids=[]
     for id in ids_list:
@@ -136,7 +137,7 @@ def get_unique_user_logins_from_projects():
     return users_ids_set
 
 def get_summary_set():
-    set_groups = get_unique_user_logins_from_groups()
+    set_groups = get_unique_user_ids_from_groups()
     set_projects = get_unique_user_logins_from_projects()
     set_summary = set_groups.union(set_projects)
     return set_summary
@@ -144,28 +145,38 @@ def get_summary_set():
 def write_unique_users_ids_to_json():
     data = get_summary_set()
     data = list(data)
-    write_to_json(data, DEFAULT_PROJECTS_UNIQUE_USERS_IDS_FILE)
+    write_to_json(data, DEFAULT_UNIQUE_USERS_IDS_FILE)
 
 def get_specific_attribute_for_all_unique_users(attribute="username"):
-    data = read_from_json(DEFAULT_PROJECTS_UNIQUE_USERS_IDS_FILE)
+    data = read_from_json(DEFAULT_UNIQUE_USERS_IDS_FILE)
     attr_dict = {}
     for id in data:
-        response = requests.get(f"{GITLAB_API_USERS_URL_BASE}/{id}")
+        attrs_dict = {}
+        response = requests.get(f"{GITLAB_API_USERS_URL_BASE}/{id}?private_token={TOKEN}")
         response_json = response.json()
-        attr_dict[id] = response_json[attribute]
+        attrs_dict[attribute] = response_json[attribute]
+        attr_dict[id] = attrs_dict
     return attr_dict
 
-def get_specific_attributes_for_all_unique_users(*attributes):
-    data = read_from_json(DEFAULT_PROJECTS_UNIQUE_USERS_IDS_FILE)
+def get_specific_attributes_for_all_unique_users(*attributes, attrs_tuple=None):
+    # if type(attributes[0]) == tuple:
+    #     attributes = attributes[0]
+    attributes = attrs_tuple if attrs_tuple else attributes
+    data = read_from_json(DEFAULT_UNIQUE_USERS_IDS_FILE)
     attr_dict = {}
     for id in data:
-        attrs_list = []
-        response = requests.get(f"{GITLAB_API_USERS_URL_BASE}/{id}")
+        attrs_dict = {}
+        response = requests.get(f"{GITLAB_API_USERS_URL_BASE}/{id}?private_token={TOKEN}")
         response_json = response.json()
         for attr in attributes:
-            attrs_list.append(response_json[attr])
-        attr_dict[id] = attrs_list
+            attrs_dict[attr] = response_json[attr]
+        attr_dict[id] = attrs_dict
     return attr_dict
+
+def write_specific_attributes_for_all_unique_users_to_json(*attributes):
+    data = get_specific_attributes_for_all_unique_users(attrs_tuple=attributes)
+    # print(data)
+    write_to_json(data, DEFAULT_USERS_IDS_FILE)
 
 # Дописать
 # projects = iterable collection (list, tuple)
@@ -198,12 +209,23 @@ def delete_user_from_all_projects(user_query, dry=False):
                     pass
         print()
 
-# print(gl.projects.get('22780804'))
-# write_all_groups_to_json()
-# write_all_projects_to_json()
-# write_all_groups_to_json()
-# print(get_groups_ids_from_json())
-# get_unique_user_logins_from_groups()
-# write_unique_users_ids_to_json()
-# print(get_specific_attribute_for_all_unique_users())
-print(get_specific_attributes_for_all_unique_users("name", "username"))
+def main():
+    # examples:
+
+    # print(gl.projects.get('22780804'))
+    # write_all_groups_to_json()
+    # write_all_projects_to_json()
+    # print(get_groups_ids_from_json())
+    # print(len(get_unique_user_ids_from_groups()))
+    # write_unique_users_ids_to_json()
+    # print(get_specific_attribute_for_all_unique_users("name"))
+    # print(get_specific_attributes_for_all_unique_users("username"))
+    # write_specific_attributes_for_all_unique_users_to_json("name", "username")
+
+    # write_all_groups_to_json()
+    # write_all_projects_to_json()
+    # write_unique_users_ids_to_json()
+    write_specific_attributes_for_all_unique_users_to_json("name", "username")
+
+if __name__ == "__main__":
+    main()
